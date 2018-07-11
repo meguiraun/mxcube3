6763b46240369d0eb936b83682363c353aea8668
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2018-07-11 11:56:05 +0200

    update config file

--- config.gui.prod.js
+++ config.gui.prod.js
@@ -1,18 +1,6 @@
 module.exports = {
 	phaseControl: true,
-	hutchControlVisible: true,
+	hutchControlVisible: false,
 	focusControlOnCanvas: true,
 	apertureControl: true,
-	beamlineCameras: [{
-		url: "http://www.youtube.com/embed/xDMP3i36naA",
-		width:"320px",
-		height:"180px",
-		location:"Experimental Hutch"
-		},
-		{
-		url: "http://www.youtube.com/embed/8CnFo7qinng",
-		width:"320px",
-		height:"180px",
-		location:"Diffractometer"
-	}]
 }
